10bda037d0873217c214e2e5cdd052c353a54c82
Mohammad Arham Imam <imamarham10.com>
2023-05-01 12:37:21 +0530

    initial commit

---
+++ src/main/java/com/example/fileawss3/FileAwsS3Application.java
@@ -0,0 +1,13 @@
+package com.example.fileawss3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileAwsS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileAwsS3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fileawss3/FileAwsS3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fileawss3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileAwsS3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
